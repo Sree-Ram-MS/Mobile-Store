afa7a487cb1c1f7d2aad4fa39b97d3e228d621a3
Heisenberg <122552247+Say-My-Name-Heisenberg@users.noreply.github.com>
2023-03-22 04:00:26 +0000

    product viewd

--- MobileStore/customer/views.py
+++ MobileStore/customer/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from store.models import Products
 
 # Create your views here.
 class CustHome(TemplateView):
     template_name="userpage.html"
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context["products"]=Products.objects.all()
+        return context
