9d4609fa40cd07fa27219aa4334734bed2e06373
Heisenberg <122552247+Say-My-Name-Heisenberg@users.noreply.github.com>
2023-03-23 04:43:12 +0000

    del

--- MobileStore/store/urls.py
+++ MobileStore/store/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("DealerHome/",DealerHome.as_view(),name='Dealer'),
     path("add/",AddProduct.as_view(),name='AddProduct'),
     path("mypro/",MyProduct.as_view(),name='MyPro'),
-    path("pro/",Profile.as_view(),name='profile')
+    path("pro/",Profile.as_view(),name='profile'),
+    path('updatepro/<int:pk>/',UpdateProduct.as_view(),name='Update'),
+    path('deletepro/<int:pk>/',RemoveProduct.as_view(),name='Remove'),
 ]
--- MobileStore/store/views.py
+++ MobileStore/store/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,CreateView
+from django.views.generic import TemplateView,CreateView,UpdateView,DeleteView
 from .models import Products
 from .forms import *
 from django.urls import reverse_lazy
 
 
 # Create your views here.
+
+# Home Page
 class DealerHome(TemplateView):
     template_name="dealerpage.html"
     def get_context_data(self, **kwargs):
@@ -13,7 +15,7 @@ class DealerHome(TemplateView):
         context["products"]=Products.objects.all()
         return context
     
-
+# Add product
 
 class AddProduct(CreateView):
     form_class=ProductForm
@@ -25,10 +27,12 @@ class AddProduct(CreateView):
         self.object=form.save()
         return super().form_valid(form)
 
+#View Profile
 
 class Profile(TemplateView):
     template_name="profile.html"
 
+#View My PRoducts
 
 class MyProduct(TemplateView):
     template_name="myproduct.html"
@@ -36,3 +40,15 @@ class MyProduct(TemplateView):
         context=super().get_context_data(**kwargs)
         context["products"]=Products.objects.filter(user=self.request.user)
         return context
+    
+class UpdateProduct(UpdateView):
+    form_class=ProductForm
+    model=Products
+    template_name='addproduct.html'
+    success_url=reverse_lazy('AddProduct')
+
+
+class RemoveProduct(DeleteView):
+    model=Products
+    template_name='delproduct.html'
+    success_url=reverse_lazy('MyPro')
